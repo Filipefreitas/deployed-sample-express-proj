8b538b72c9d2d55c9b1b0be846926735989e87c2
Filipe Freitas <filipe.cunha.freitas@gmail.com>
2021-03-09 23:42:56 -0300

    heroku adjustment

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.get("/", (req,res)=>{
     }
 })
 
-const PORT = 3000;
+// the || operator is used to maintain the 3000 port acessible when running locally
+const PORT = process.env.PORT || 3000;
 app.listen(3000, ()=>{
     console.log("Web site is up and running")
 })
